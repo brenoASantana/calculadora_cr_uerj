3a1d9b720a2da59a1e453fda170dc6a48bad3f10
Breno Santana <contatobrenosantana@outlook.com>
2023-10-25 23:54:48 -0300

    Comment importante

--- main.py
+++ main.py
@@ -26,3 +26,9 @@ if confirm == True:
     cr(credMat, mediaMat)
 else:
     mediaMat = int(input("Qual dado você deseja alterar?"))
+
+## O cálculo do Coeficiente de Rendimento (CR) é feito para os estudantes do Regime de Crédito com o objetivo de classificá-los dentro do curso. Essa classificação estabelece prioridades no preenchimento das vagas nas disciplinas/turmas escolhidas. Para calcular o CR, utiliza-se a seguinte fórmula:
+## CR = Somatório (n° de créditos  X nota) / Somatório (nº de créditos)
+## NUMERADOR: Somatório dos produtos dos créditos de cada disciplina pela respectiva nota, tanto na aprovação como na reprovação por nota ou frequência.
+## DENOMINADOR: Cálculo do somatório dos créditos.
+## No cálculo do CR não são consideradas as disciplinas com situação de isenção, aprovação sem nota e inscrição cancelada.
