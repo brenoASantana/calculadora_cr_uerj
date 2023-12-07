63eb3fef67fcbfb27ee9dbdffd7e3f4f15761213
Breno Santana <contatobrenosantana@outlook.com>
2023-12-07 10:44:39 -0300

    Algoritmo funcionando

--- main.py
+++ main.py
@@ -1,55 +1,108 @@
 import tkinter
 
+## Implementar um arquivo executável?
+
+
 def telaInicial():
-    print(
-        """
-        Seja bem-vinde à calculadora de Coeficiente de Rendimento (CR) da UERJ!
-        1 - Iniciar
-        2 - Saber mais sobre como é calculado o CR
-        3 - Extra
-        0 - Sair
-        """        
+    op = int(
+        input(
+            """
+1 - Iniciar
+2 - Saber mais sobre como é calculado o CR
+3 - Extra
+                  
+0 - Sair
+"""
+        )
     )
-    return int(input())
+
+    return op
 
 
-def cr():
-    return None
+def cr(quantMat, creditos, medias):
+    ## O cálculo do Coeficiente de Rendimento (CR) é feito para os estudantes do Regime de Crédito com o objetivo de classificá-los dentro do curso. Essa classificação estabelece prioridades no preenchimento das vagas nas disciplinas/turmas escolhidas. Para calcular o CR, utiliza-se a seguinte fórmula:
+    ## CR = Somatório (n° de créditos  X nota) / Somatório (nº de créditos)
+    ## NUMERADOR: Somatório dos produtos dos créditos de cada disciplina pela respectiva nota, tanto na aprovação como na reprovação por nota ou frequência.
+    ## DENOMINADOR: Cálculo do somatório dos créditos.
+    ## No cálculo do CR não são consideradas as disciplinas com situação de isenção, aprovação sem nota e inscrição cancelada.
 
-op = telaInicial()
+    numerador = 0.0
+    denominador = 0.0
+    ## salvar em arquivo?
+    ##Calcular o numerador e denominador
+    for i in range(quantMat):
+        credito = creditos[i]
+        media = medias[i]
+        numerador += credito * media
+        denominador += credito
 
-match op:
-    case 1:
-        quantMat = int(input("Quantas matérias você inscreveu-se nesse semestre?\n"))
+    cr = numerador / denominador
 
-        materias = [""]*quantMat
-
-        notas = [0.0]*quantMat
-        medias = [0.0]*quantMat
-        creditos  = [0]*quantMat
-
-        ## matriz para os dados?
-
-        for i in range(quantMat):
-            materias[i] = str(input(f"Qual o nome da {i}º matéria?\n"))
-            ## Talvez fazer um banco de dados com os nomes das materias e dos institutos?
-            creditos = int(input(f"Quantas créditos a matéria de {nomeMat} possuí?\n"))
-            notas = int(input(f"Qual foi sua nota na matéria de {nomeMat}?\n"))
-            medias = int(input(f"Qual média da matéria de {nomeMat}?\n"))
-            isConfirm = bool(input("Todas as informações acima estão corretas?\n"))
-
-        if isConfirm == True:
-            cr(credMat, mediaMat)
-        else:
-            mediaMat = int(input("Qual dado você deseja alterar?"))
+    return cr
 
 
-    case 2:
-        print()
-## mostra a imagem crscreen
+quantMat = 0
 
-## O cálculo do Coeficiente de Rendimento (CR) é feito para os estudantes do Regime de Crédito com o objetivo de classificá-los dentro do curso. Essa classificação estabelece prioridades no preenchimento das vagas nas disciplinas/turmas escolhidas. Para calcular o CR, utiliza-se a seguinte fórmula:
-## CR = Somatório (n° de créditos  X nota) / Somatório (nº de créditos)
-## NUMERADOR: Somatório dos produtos dos créditos de cada disciplina pela respectiva nota, tanto na aprovação como na reprovação por nota ou frequência.
-## DENOMINADOR: Cálculo do somatório dos créditos.
-## No cálculo do CR não são consideradas as disciplinas com situação de isenção, aprovação sem nota e inscrição cancelada.
+
+print("Seja bem-vinde à calculadora de Coeficiente de Rendimento (CR) da UERJ!")
+
+while True:
+    op = telaInicial()
+
+    match op:
+        ## Iniciar
+        case 1:
+            quantMat = int(
+                input("Quantas matérias você inscreveu-se nesse semestre?\n")
+            )
+
+            nomesMaterias = [""] * quantMat
+            creditos = [0] * quantMat
+            medias = [0.0] * quantMat
+
+            for i in range(quantMat):
+                nomeMateria = str(input(f"Qual o nome da {i+1}º matéria?\n"))
+
+                credito = int(
+                    input(f"Quantos créditos a matéria de {nomeMateria} possuí?\n")
+                )
+
+                media = float(
+                    input(f"Qual foi sua média na matéria de {nomeMateria}?\n")
+                )
+
+                isConfirm = int(
+                    input("Todas as informações acima estão corretas? 1-Sim 2-Não\n")
+                )
+
+                ## Primeiro confirma se os dados estão certo antes de adicioná-los no vetor
+                if isConfirm == 1:
+                    nomesMaterias[i] = nomeMateria
+                    creditos[i] = credito
+                    medias[i] = media
+                else:
+                    ## Pensar em como retonar pro case 1 e não o processo todo
+                    ## pois essa parte está equivalente ao 0 - Sair
+                    print("Apagando os dados e reiniciando o processo...")
+                    continue
+
+            cr = cr(quantMat, creditos, medias)
+            print(f"Seu CR este semestre é: {cr}")
+            continue
+        ## Sobre
+        case 2:
+            print()
+        ## mostra a imagem crscreen
+
+        ## Extra
+        case 3:
+            print()
+
+        ## Sair
+        case 0:
+            print("Saindo do programa...")
+            break
+
+        case _:
+            print("Por favor, selecione uma das opções disponíveis.")
+            continue
