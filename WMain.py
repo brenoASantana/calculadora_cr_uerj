7e6671f0f3d89966b6ac1cbcfd12601d80b980dc
Breno Santana <contatobrenosantana@outlook.com>
2024-02-27 22:12:06 -0300

    Correção erros e comment em coisas a arrumar

--- WMain.py
+++ WMain.py
@@ -1,149 +1,147 @@
 from tkinter import *
 from PIL import ImageTk
 
-class WMain:
-    #! Processo realizado por interface gráfica!
+#! Processo realizado por interface gráfica!
+global window
+window = Tk()
+numSubjects = None
+
+
+def cr(numSubjects, subCredits, averages):
+
+    numerator = 0.0
+    denominator = 0.0
+
+    for i in range(numSubjects):
+        credit = subCredits[i]
+        average = averages[i]
+        numerator += credit * average
+        denominator += credit
+
+    cr = numerator / denominator
+
+    return cr
+
+
+def skipLine(col, line):
+    return Label(window, text="").grid(column=col, row=line)
+
+
+def index():
+    global window
+    window.destroy()
     window = Tk()
-    numSubjects = None
+    window.title("Calculadora CR UERJ")
 
-    def cr(numSubjects, credits, averages):
+    skipLine(1, 0)
 
-        numerator = 0.0
-        denominator = 0.0
+    Label(
+        window,
+        text="Seja bem-vinde à calculadora de Coeficiente de Rendimento (CR) da UERJ!",
+    ).grid(column=1, row=1)
 
-        for i in range(numSubjects):
-            credit = subCredits[i]
-            average = averages[i]
-            numerator += credit * average
-            denominator += credit
+    skipLine(1, 2)
+    Button(window, text="Iniciar", command=numSubjects).grid(column=1, row=3)
+    Button(window, text="Como é calculado o CR da UERJ?", command=aboutCR).grid(
+        column=1, row=4
+    )
+    Button(window, text="Extra", command="").grid(column=1, row=5)
 
-        cr = numerator / denominator
-
-        return cr
-
-    def skipLine(col, line):
-        return Label(window, text="").grid(column=col, row=line)
+    skipLine(1, 6)
+    window.mainloop()
 
 
-    def index():
-        global window
-        window.destroy()
-        window = Tk()
-        window.title("Calculadora CR UERJ")
+def aboutCR():
+    global window
+    window.destroy()
+    window = Tk()
+    window.title("Sobre o CR")
 
-        skipLine(1, 0)
+    crScreenImage = ImageTk.PhotoImage(file="crscreen.jpg")
 
-        Label(
-            window,
-            text="Seja bem-vinde à calculadora de Coeficiente de Rendimento (CR) da UERJ!",
-        ).grid(column=1, row=1)
+    Label(window, image=crScreenImage).grid(column=1, row=0)
 
-        skipLine(1, 2)
-        Button(window, text="Iniciar", command=numSubjects).grid(column=1, row=3)
-        Button(window, text="Como é calculado o CR da UERJ?", command=aboutCR).grid(
-            column=1, row=4
+    skipLine(1, 1)
+    Button(window, text="Voltar", command=index).grid(column=1, row=2)
+
+    skipLine(1, 3)
+
+    window.mainloop()
+
+
+def numSubjects():
+    global window
+    window.destroy()
+    window = Tk()
+    window.title("Calculando CR")
+    skipLine(1, 0)
+    Label(window, text="Quantas matérias você inscreveu-se nesse semestre?").grid(
+        column=1, row=1
+    )
+    global numSubjects
+    numSubjects = Entry(window)
+    numSubjects.grid(column=1, row=3)
+    skipLine(1, 4)
+    Button(window, text="Avançar", command=subjects).grid(column=1, row=5)
+    skipLine(1, 6)
+    window.mainloop()
+
+
+def subjects():
+    global numSubjects
+    numSubjects = numSubjects.get()
+
+    subjectNames = [""] * numSubjects
+    subjectCredits = [0] * numSubjects
+    subjectAverages = [0.0] * numSubjects
+
+    global window
+    window.destroy()
+    window = Tk()
+    window.title("Calculando CR")
+    skipLine(1, 0)
+    Label(window, text="a").grid(column=1, row=1)
+
+    iError = -1
+    i = 0
+    # TODO: Converter algoritmo para o tkinter
+    # TODO: Planejar se informa os dados em janelas separadas ou tudo na mesma janela
+    ## Um while no lugar de um for para poder retornar o valor do indice caso o usuário queira corrigir os dados
+    while i < numSubjects:
+        nameSubject = str(input(f"Qual o nome da {i+1}º matéria?\n"))
+
+        ## credit = int(input(f"Quantos créditos a matéria de {nomeMateria} possuí?\n"))
+
+        ## average = float(input(f"Qual foi sua média na matéria de {nomeMateria}?\n"))
+
+        isConfirm = int(
+            input("Todas as informações acima estão corretas? 1-Sim 2-Não\n")
         )
-        Button(window, text="Extra", command="").grid(column=1, row=5)
 
-        skipLine(1, 6)
-        window.mainloop()
+        ## Primeiro confirma se os dados estão certo antes de adicioná-los no vetor
+        if isConfirm == 1:
+            print("")
+        ##    nomesMaterias[i] = nomeMateria
+        ##    creditos[i] = credito
+        ##    medias[i] = media
 
+        else:
+            print("OK. Vamos tentar novamente...")
+            ## Volta uma posição para sobrescrever os dados
+            i -= 1
+        i += 1
 
-    def aboutCR():
-        global window
-        window.destroy()
-        window = Tk()
-        window.title("Sobre o CR")
+    ## cr = cr(quantMat, creditos, medias)
+    ## Perguntar se desejar gerar um arquivo com os dados
+    print(f"Seu CR este semestre é: {cr:.2f}")
 
-        crScreenImage = ImageTk.PhotoImage(file="crscreen.jpg")
+    ## ou continue?
 
-        Label(window, image=crScreenImage).grid(column=1, row=0)
-
-        skipLine(1, 1)
-        Button(window, text="Voltar", command=index).grid(column=1, row=2)
-
-        skipLine(1, 3)
-
-        window.mainloop()
-
-
-    def numSubjects():
-        global window
-        window.destroy()
-        window = Tk()
-        window.title("Calculando CR")
-        skipLine(1, 0)
-        Label(window, text="Quantas matérias você inscreveu-se nesse semestre?").grid(
-            column=1, row=1
-        )
-        global numSubjects
-        numSubjects = Entry(window)
-        numSubjects.grid(column=1, row=3)
-        skipLine(1, 4)
-        Button(window, text="Avançar", command=subjects).grid(column=1, row=5)
-        skipLine(1, 6)
-        window.mainloop()
+    # * Ordem para calcular CR:
+    # * 1. Nº de Disciplinas
+    # * 2. Nome, Crédito e Média na Disciplina
+    # * 3. Resultado do CR
 
 
-    def subjects():    
-        global numSubjects
-        numSubjects = numSubjects.get()
-        
-        subjectNames = [""] * numSubjects
-        subjectCredits = [0] * numSubjects
-        subjectAverages = [0.0] * numSubjects
-        
-        global window
-        window.destroy()
-        window = Tk()
-        window.title("Calculando CR")
-        skipLine(1, 0)
-        Label(window, text="a").grid(column=1, row=1)
-        
-        iError = -1
-        i = 0
-        # TODO: Converter algoritmo para o tkinter
-        # TODO: Planejar se informa os dados em janelas separadas ou tudo na mesma janela
-        ## Um while no lugar de um for para poder retornar o valor do indice caso o usuário queira corrigir os dados
-        while i < numSubjects:
-                nameSubject = str(input(f"Qual o nome da {i+1}º matéria?\n"))
-
-                credit = int(
-                        input(f"Quantos créditos a matéria de {nomeMateria} possuí?\n")
-                    )
-
-                average = float(
-                        input(f"Qual foi sua média na matéria de {nomeMateria}?\n")
-                    )
-
-                isConfirm = int(
-                        input("Todas as informações acima estão corretas? 1-Sim 2-Não\n")
-                    )
-
-                ## Primeiro confirma se os dados estão certo antes de adicioná-los no vetor
-                if isConfirm == 1:
-                    nomesMaterias[i] = nomeMateria
-                    creditos[i] = credito
-                    medias[i] = media
-
-                else:
-                    print("OK. Vamos tentar novamente...")
-                    ## Volta uma posição para sobrescrever os dados
-                    i -= 1
-                i += 1
-
-        cr = cr(quantMat, creditos, medias)
-        ## Perguntar se desejar gerar um arquivo com os dados
-        print(f"Seu CR este semestre é: {cr:.2f}")
-                
-
-                ## ou continue?
-                
-        # * Ordem para calcular CR:
-        # * 1. Nº de Disciplinas
-        # * 2. Nome, Crédito e Média na Disciplina
-        # * 3. Resultado do CR
-
-
-    # chamada de procedimento que inicia o processo
-    index()
+# chamada de procedimento que inicia o processo
+index()
--- main.py
+++ main.py
@@ -1,123 +1,122 @@
-class Main:
-    #! Processo realizado pelo terminal!
-    def telaInicial():
-        op = int(
-            input(
-                """
-    1 - Iniciar
-    2 - Saber mais sobre como é calculado o CR
-    3 - Extra
-                    
-    0 - Sair
-    """
-            )
+#! Processo realizado pelo terminal!
+def telaInicial():
+    op = int(
+        input(
+            """
+1 - Iniciar
+2 - Saber mais sobre como é calculado o CR
+3 - Extra
+                
+0 - Sair
+"""
         )
+    )
 
-        return op
+    return op
 
-    def cr(quantMat, creditos, medias):
-        ## O cálculo do Coeficiente de Rendimento (CR) é feito para os estudantes do Regime de Crédito com o objetivo de classificá-los dentro do curso. Essa classificação estabelece prioridades no preenchimento das vagas nas disciplinas/turmas escolhidas. Para calcular o CR, utiliza-se a seguinte fórmula:
-        ## CR = Somatório (n° de créditos  X nota) / Somatório (nº de créditos)
-        ## NUMERADOR: Somatório dos produtos dos créditos de cada disciplina pela respectiva nota, tanto na aprovação como na reprovação por nota ou frequência.
-        ## DENOMINADOR: Cálculo do somatório dos créditos.
-        ## No cálculo do CR não são consideradas as disciplinas com situação de isenção, aprovação sem nota e inscrição cancelada.
 
-        numerador = 0.0
-        denominador = 0.0
+def cr(quantMat, creditos, medias):
+    ## O cálculo do Coeficiente de Rendimento (CR) é feito para os estudantes do Regime de Crédito com o objetivo de classificá-los dentro do curso. Essa classificação estabelece prioridades no preenchimento das vagas nas disciplinas/turmas escolhidas. Para calcular o CR, utiliza-se a seguinte fórmula:
+    ## CR = Somatório (n° de créditos  X nota) / Somatório (nº de créditos)
+    ## NUMERADOR: Somatório dos produtos dos créditos de cada disciplina pela respectiva nota, tanto na aprovação como na reprovação por nota ou frequência.
+    ## DENOMINADOR: Cálculo do somatório dos créditos.
+    ## No cálculo do CR não são consideradas as disciplinas com situação de isenção, aprovação sem nota e inscrição cancelada.
 
-        for i in range(quantMat):
-            credito = creditos[i]
-            media = medias[i]
-            numerador += credito * media
-            denominador += credito
+    numerador = 0.0
+    denominador = 0.0
 
-        cr = numerador / denominador
+    for i in range(quantMat):
+        credito = creditos[i]
+        media = medias[i]
+        numerador += credito * media
+        denominador += credito
 
-        return cr
+    cr = numerador / denominador
 
-    quantMat = 0
-    isGerarArqRes = False
+    return cr
 
-    while True:
-        print("Seja bem-vinde à calculadora de Coeficiente de Rendimento (CR) da UERJ!")
-        op = telaInicial()
-        match op:
-            ## Iniciar
-            case 1:
-                GerarArqRes = int(
-                    input(
-                        "Você deseja gerar um arquivo de texto com os dados e o resultado? 1-Sim 2-Não\n"
-                    )
+
+quantMat = 0
+isGerarArqRes = False
+
+while True:
+    print("Seja bem-vinde à calculadora de Coeficiente de Rendimento (CR) da UERJ!")
+    op = telaInicial()
+    match op:
+        ## Iniciar
+        case 1:
+            GerarArqRes = int(
+                input(
+                    "Você deseja gerar um arquivo de texto com os dados e o resultado? 1-Sim 2-Não\n"
                 )
-                if GerarArqRes == 1:
-                    file = open("Resultado_CR.txt", "w")
-                    isGerarArqRes = True
+            )
+            if GerarArqRes == 1:
+                file = open("Resultado_CR.txt", "w")
+                isGerarArqRes = True
 
-                quantMat = int(
-                    input("Quantas matérias você inscreveu-se nesse semestre?\n")
+            quantMat = int(
+                input("Quantas matérias você inscreveu-se nesse semestre?\n")
+            )
+
+            nomesMaterias = [""] * quantMat
+            creditos = [0] * quantMat
+            medias = [0.0] * quantMat
+
+            iError = -1
+            i = 0
+
+            ## Um while no lugar de um for para poder retornar o valor do indice caso o usuário queira corrigir os dados
+            while i < quantMat:
+                nomeMateria = str(input(f"Qual o nome da {i+1}º matéria?\n"))
+
+                credito = int(
+                    input(f"Quantos créditos a matéria de {nomeMateria} possuí?\n")
                 )
 
-                nomesMaterias = [""] * quantMat
-                creditos = [0] * quantMat
-                medias = [0.0] * quantMat
+                media = float(
+                    input(f"Qual foi sua média na matéria de {nomeMateria}?\n")
+                )
 
-                iError = -1
-                i = 0
+                isConfirm = int(
+                    input("Todas as informações acima estão corretas? 1-Sim 2-Não\n")
+                )
 
-                ## Um while no lugar de um for para poder retornar o valor do indice caso o usuário queira corrigir os dados
-                while i < quantMat:
-                    nomeMateria = str(input(f"Qual o nome da {i+1}º matéria?\n"))
+                ## Primeiro confirma se os dados estão certo antes de adicioná-los no vetor
+                if isConfirm == 1:
+                    nomesMaterias[i] = nomeMateria
+                    creditos[i] = credito
+                    medias[i] = media
 
-                    credito = int(
-                        input(f"Quantos créditos a matéria de {nomeMateria} possuí?\n")
-                    )
+                else:
+                    print("OK. Vamos tentar novamente...")
+                    ## Volta uma posição para sobrescrever os dados
+                    i -= 1
+                i += 1
 
-                    media = float(
-                        input(f"Qual foi sua média na matéria de {nomeMateria}?\n")
-                    )
+            cr = cr(quantMat, creditos, medias)
+            ## Perguntar se desejar gerar um arquivo com os dados
+            print(f"Seu CR este semestre é: {cr:.2f}")
 
-                    isConfirm = int(
-                        input(
-                            "Todas as informações acima estão corretas? 1-Sim 2-Não\n"
-                        )
-                    )
+            if isGerarArqRes == True:
+                file.closed
+            ## ou continue?
+            break
+        ## Sobre
+        case 2:
+            print("sobre")
+            continue
+        ## mostra a imagem crscreen
 
-                    ## Primeiro confirma se os dados estão certo antes de adicioná-los no vetor
-                    if isConfirm == 1:
-                        nomesMaterias[i] = nomeMateria
-                        creditos[i] = credito
-                        medias[i] = media
+        ## Extra
+        case 3:
+            print("extra")
+            continue
 
-                    else:
-                        print("OK. Vamos tentar novamente...")
-                        ## Volta uma posição para sobrescrever os dados
-                        i -= 1
-                    i += 1
+        ## Sair
+        case 0:
+            print("Saindo do programa...")
+            break
 
-                cr = cr(quantMat, creditos, medias)
-                ## Perguntar se desejar gerar um arquivo com os dados
-                print(f"Seu CR este semestre é: {cr:.2f}")
-
-                if isGerarArqRes == True:
-                    file.closed
-                ## ou continue?
-                break
-            ## Sobre
-            case 2:
-                print("sobre")
-                continue
-            ## mostra a imagem crscreen
-
-            ## Extra
-            case 3:
-                print("extra")
-                continue
-
-            ## Sair
-            case 0:
-                print("Saindo do programa...")
-                break
-
-            case _:
-                print("Por favor, selecione uma das opções disponíveis.")
-                continue
+        case _:
+            print("Por favor, selecione uma das opções disponíveis.")
+            continue
